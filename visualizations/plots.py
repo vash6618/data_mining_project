8b2552f2011815bf03e7e9d7620c2aff1b27190c
vash6618 <vasusharma@Vasus-MacBook-Pro.local>
2021-07-15 01:01:52 -0600

    Race correlation with income

--- visualizations/plots.py
+++ visualizations/plots.py
@@ -6,19 +6,21 @@ from collections import defaultdict
 
 region_map = {32: 'East South', 42: 'Pacific', 41: 'Mountain', 33: 'West South', 11: 'New England', 31: 'South Atlantic',
               21: 'East North', 22: 'West North', 12: 'Middle Atlantic'}
+race_map = {1: 'White', 2: 'Black/African American', 3: 'American Indian', 4: 'Chinese', 5: 'Japanese',
+            6: 'Pacific Islander', 7: 'Other race', 8: 'Biracial', 9: 'Three major races'}
 
-def income_vs_region(df):
+def income_vs_graph(df, attribute):
     print("vs")
-    wage_income = df['INCTOT'].dropna()
-    region = df['REGION'].dropna()
-    tot_inc = wage_income.to_list()
-    reg = region.to_list()
+    total_income = df['INCTOT'].dropna()
+    attr = df[attribute].dropna()
+    tot_inc = total_income.to_list()
+    attr = attr.to_list()
     dp = defaultdict(list)
 
     for i in range(len(tot_inc)):
         if tot_inc[i] == 999999 or tot_inc[i] == 999998:
             continue
-        dp[reg[i]].append(tot_inc[i])
+        dp[attr[i]].append(tot_inc[i])
     median_wage = {}
     for key in dp:
         dp[key].sort()
@@ -26,18 +28,24 @@ def income_vs_region(df):
     print(median_wage)
 
     x_values = list(median_wage.keys())
-    x_values = [region_map[val] for val in x_values]
+    if attribute == 'REGION':
+        x_values = [region_map[val] for val in x_values]
+    else:
+        x_values = [race_map[val] for val in x_values]
     y_values = [median_wage[key] for key in median_wage]
     ax = sns.barplot(x=x_values, y=y_values)
-    ax.set(xlabel='Region', ylabel='income ($)')
-    plt.title('Yearly Income vs Region')
+    ax.set(xlabel='Race' if attribute == 'RACE' else 'Region', ylabel='income ($)')
+    title = 'Income Earned vs Race' if attribute == 'RACE' else 'Income Earned vs Region'
+    plt.title(title)
     plt.show()
+
 
 
 if __name__ == '__main__':
     file_name = '~/Downloads/2019_census_dataset.csv'
     df = pd.read_csv(file_name)
     print(df.loc[0])
-    income_vs_region(df)
+    # income_vs_region(df)
+    income_vs_graph(df, 'RACE')
 
 
